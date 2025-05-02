f9679cfc154083d71708a5b7ca08d7a5b6994779
Mario Gomes <mario.gomes@liferay.com>
2025-05-02 08:22:51 -0300

    fix problem

--- h7g5-web/src/main/java/com/liferay/h7g5/web/internal/portlet/H7G5Portlet.java
+++ h7g5-web/src/main/java/com/liferay/h7g5/web/internal/portlet/H7G5Portlet.java
@@ -1,5 +1,9 @@
 package com.liferay.h7g5.web.internal.portlet;
 
+import com.liferay.h7g5.model.H7G5Folder;
+import com.liferay.h7g5.service.H7G5FolderLocalService;
+import com.liferay.portal.kernel.util.StringUtil;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
@@ -9,6 +13,7 @@ import javax.portlet.RenderRequest;
 import javax.portlet.RenderResponse;
 
 import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
 @Component(
 	property = {
@@ -33,6 +38,25 @@ public class H7G5Portlet extends GenericPortlet {
 		PrintWriter printWriter = renderResponse.getWriter();
 
 		printWriter.println("Hello, H7G5!");
+
+		System.out.println(
+			"There are " + _h7G5FolderLocalService.getH7G5FoldersCount() +
+				" folders.");
+
+		H7G5Folder h7g5Folder = _h7G5FolderLocalService.createH7G5Folder(
+			System.currentTimeMillis());
+
+		h7g5Folder.setDescription(StringUtil.randomString());
+		h7g5Folder.setName(StringUtil.randomString());
+
+		_h7G5FolderLocalService.addH7G5Folder(h7g5Folder);
+
+		System.out.println(
+			"After adding a new folder, there are now " +
+				_h7G5FolderLocalService.getH7G5FoldersCount() + " folders.");
 	}
+
+	@Reference
+	private H7G5FolderLocalService _h7G5FolderLocalService;
 
 }
